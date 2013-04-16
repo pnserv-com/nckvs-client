d38faea470d8f5eeb4fd2a46695d38a58cd971ef
Yoshihisa Tanaka <yoshihisa@iij.ad.jp>
2013-04-16 15:53:36 +0900

    add the KVSClient class which is replaced module level functions

--- nckvsclient/__init__.py
+++ nckvsclient/__init__.py
@@ -9,10 +9,6 @@ except ImportError:
     import urllib2 as request
 
 
-_config = {}
-_system_param = {}
-
-
 class RPCError(Exception):
     def __init__(self, code, message):
         self.code = code
@@ -22,57 +18,53 @@ class RPCError(Exception):
         return 'RPCError: {} {}'.format(self.code, self.message)
 
 
-def setup(base_url, login_name, login_pass, **kwargs):
-    _config.clear()
-    _system_param.clear()
+class KVSClient(object):
+    def __init__(self, base_url, login_name, login_pass, **kwargs):
+        self.config = dict(base_url=base_url,
+                           login_name=login_name,
+                           login_pass=login_pass,
+                           **kwargs)
+        self.system_param = {}
 
-    _config['base_url'] = base_url
-    _config['login_name'] = login_name
-    _config['login_pass'] = login_pass
-    _config.update(kwargs)
+        for key in ('login_name', 'login_pass', 'app_servername',
+                    'app_username', 'timezone'):
+            self.system_param[key] = self.config.get(key, '')
 
-    for key in ('login_name', 'login_pass', 'app_servername', 'app_username',
-                'timezone'):
-        _system_param[key] = _config.get(key, '')
-
-
-def set(items):
-    url = _config['base_url'] + '/data/set/'
-    param = {
-        'system': _system_param,
-        'query': {
-            'datalist': items,
-            'datatypename': 'commonstest1',
-            'datatypeversion': 1
+    def set(self, items):
+        url = self.config['base_url'] + '/data/set/'
+        param = {
+            'system': self.system_param,
+            'query': {
+                'datalist': items,
+                'datatypename': 'commonstest1',
+                'datatypeversion': 1
+            }
         }
-    }
-    return _request(url, param)
+        return self._request(url, param)
 
-
-def search(query):
-    url = _config['base_url'] + '/data/search/'
-    param = {
-        'system': _system_param,
-        'query': {
-            'datatypename': 'commonstest1',
-            'dataversion': '*',
-            'limit': 0,
-            'sortorder': [],
-            'matching': query
+    def search(self, query):
+        url = self.config['base_url'] + '/data/search/'
+        param = {
+            'system': self.system_param,
+            'query': {
+                'datatypename': 'commonstest1',
+                'dataversion': '*',
+                'limit': 0,
+                'sortorder': [],
+                'matching': query
+            }
         }
-    }
-    return _request(url, param)
+        return self._request(url, param)
 
-
-def _request(url, param):
-    data = json.dumps(param)
-    headers = {
-        'Content-type': 'application/json',
-        'Content-length': len(data)
-    }
-    req = request.Request(url, data, headers)
-    with closing(request.urlopen(req)) as res:
-        result = json.load(res)
-        if result['code'] == '200':
-            return result
-        raise RPCError(result['code'], result['message'])
+    def _request(self, url, param):
+        data = json.dumps(param)
+        headers = {
+            'Content-type': 'application/json',
+            'Content-length': len(data)
+        }
+        req = request.Request(url, data, headers)
+        with closing(request.urlopen(req)) as res:
+            result = json.load(res)
+            if result['code'] == '200':
+                return result
+            raise RPCError(result['code'], result['message'])
--- tests/test_nckvsclient.py
+++ tests/test_nckvsclient.py
@@ -19,16 +19,6 @@ except ImportError:
 BASE_URL = 'http://example.com'
 
 
-def setup_module(module):
-    nckvs.setup(BASE_URL, 'user', 'pass', app_servername='appname',
-                app_username='appuser', timezone='')
-
-
-def teardown_module(module):
-    nckvs._config.clear()
-    nckvs._system_param.clear()
-
-
 def pytest_funcarg__system_param(request):
     return {
         'login_name': 'user',
@@ -39,67 +29,69 @@ def pytest_funcarg__system_param(request):
     }
 
 
-def test_setup(system_param):
-    assert nckvs._config == {
-        'base_url': BASE_URL,
-        'login_name': 'user',
-        'login_pass': 'pass',
-        'app_servername': 'appname',
-        'app_username': 'appuser',
-        'timezone': ''
-    }
-    assert nckvs._system_param == system_param
+def pytest_funcarg__client(request):
+    return nckvs.KVSClient(BASE_URL, 'user', 'pass', app_servername='appname',
+                           app_username='appuser', timezone='')
 
 
-@patch('nckvsclient._request')
-def test_set(_request, system_param):
-    nckvs.set([{'key1': 'value1'}])
-    assert _request.call_args == call(BASE_URL + '/data/set/', {
-        'system': system_param,
-        'query': {
-            'datalist': [{'key1': 'value1'}],
-            'datatypename': 'commonstest1',
-            'datatypeversion': 1
+class TestKVSClient(object):
+    def test_init(self, client, system_param):
+        assert client.config == {
+            'base_url': BASE_URL,
+            'login_name': 'user',
+            'login_pass': 'pass',
+            'app_servername': 'appname',
+            'app_username': 'appuser',
+            'timezone': ''
         }
-    })
+        assert client.system_param == system_param
 
+    @patch('nckvsclient.KVSClient._request')
+    def test_set(self, _request, client, system_param):
+        client.set([{'key1': 'value1'}])
+        assert _request.call_args == call(BASE_URL + '/data/set/', {
+            'system': system_param,
+            'query': {
+                'datalist': [{'key1': 'value1'}],
+                'datatypename': 'commonstest1',
+                'datatypeversion': 1
+            }
+        })
 
-@patch('nckvsclient._request')
-def test_search(_request, system_param):
-    nckvs.search([{'key': 'key1', 'value': 'valu1', 'pattern': 'cmp'}])
-    assert _request.call_args == call(BASE_URL + '/data/search/', {
-        'system': system_param,
-        'query': {
-            'datatypename': 'commonstest1',
-            'dataversion': '*',
-            'limit': 0,
-            'sortorder': [],
-            'matching': [{'key': 'key1', 'value': 'valu1', 'pattern': 'cmp'}]
+    @patch('nckvsclient.KVSClient._request')
+    def test_search(self, _request, client, system_param):
+        client.search([{'key': 'key1', 'value': 'valu1', 'pattern': 'cmp'}])
+        assert _request.call_args == call(BASE_URL + '/data/search/', {
+            'system': system_param,
+            'query': {
+                'datatypename': 'commonstest1',
+                'dataversion': '*',
+                'limit': 0,
+                'sortorder': [],
+                'matching': [{'key': 'key1', 'value': 'valu1', 'pattern': 'cmp'}]
+            }
+        })
+
+    @patch.object(request, 'urlopen')
+    def test_request(self, urlopen, client):
+        urlopen.return_value = StringIO('{"code":"200"}')
+        res = client._request(BASE_URL, {'key': 'value'})
+        assert res == {'code': '200'}
+        req = urlopen.call_args[0][0]
+        assert req.data == '{"key": "value"}'
+        assert req.headers == {
+            'Content-type': 'application/json',
+            'Content-length': 16
         }
-    })
 
-
-@patch.object(request, 'urlopen')
-def test_request(urlopen):
-    urlopen.return_value = StringIO('{"code":"200"}')
-    res = nckvs._request(BASE_URL, {'key': 'value'})
-    assert res == {'code': '200'}
-    req = urlopen.call_args[0][0]
-    assert req.data == '{"key": "value"}'
-    assert req.headers == {
-        'Content-type': 'application/json',
-        'Content-length': 16
-    }
-
-
-@patch.object(request, 'urlopen')
-def test_request_error(urlopen):
-    urlopen.return_value = StringIO('{"code":"400","message":"invalid"}')
-    with pytest.raises(nckvs.RPCError):
-        try:
-            nckvs._request(BASE_URL, {'key': 'value'})
-        except nckvs.RPCError as e:
-            assert e.code == '400'
-            assert e.message == 'invalid'
-            assert str(e) == 'RPCError: 400 invalid'
-            raise
+    @patch.object(request, 'urlopen')
+    def test_request_error(self, urlopen, client):
+        urlopen.return_value = StringIO('{"code":"400","message":"invalid"}')
+        with pytest.raises(nckvs.RPCError):
+            try:
+                client._request(BASE_URL, {'key': 'value'})
+            except nckvs.RPCError as e:
+                assert e.code == '400'
+                assert e.message == 'invalid'
+                assert str(e) == 'RPCError: 400 invalid'
+                raise
